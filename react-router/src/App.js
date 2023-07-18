7d5e44c5807cdfa7b2d41f31fcc8af5cc4b00396
SpotLight226 <wkdqhfl7@gmail.com>
2023-07-18 11:07:46 +0900

    react publish

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,13 +1,17 @@
-import A from "./component3/A";
-import B from "./component3/B";
-import { UserProvider } from "./context/ContextAPI2";
+import { Route, Routes } from "react-router-dom";
+import NewsHome from "./component2/NewsHome";
+import Header from "./layout/Header";
+
 const App = () => {
   return (
-    // return <UserContext.Provider value={value}>{children}</UserContext.Provider>; 여기서 A가 children
-    <UserProvider>
-      <A />
-      <B />
-    </UserProvider>
+    <Routes>
+      <Route element={<Header />}>
+        <Route path="/" element={<NewsHome />} />
+        {/* 페이지를 이동하지 않고, 밑에 NewHome을 띄운다 
+						/:<가변적으로 변하는 값>*/}
+        <Route path="/:category" element={<NewsHome />} />
+      </Route>
+    </Routes>
   );
 };
 
